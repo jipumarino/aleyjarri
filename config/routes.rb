ecc41aacf374877c7ba4d48b5ac3e20481620a77
Augusto Larraín <jipumarino@gmail.com>
2011-11-12 09:42:25 -0300

    Ruta /

--- config/routes.rb
+++ config/routes.rb
@@ -50,7 +50,7 @@ Aleyjarri::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'invitaciones#index'
 
   # See how all your routes lay out with "rake routes"
 
